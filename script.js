f08b4807cc526b90fbff6a2d775d7bb61bec4c43
okoisorjr <okoisorjr@gmail.com>
2025-11-14 11:42:56 +0100

    integration of responsive dashboard

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
 const menuButton = document.getElementById("menuButton");
+const closeButton = document.getElementById("closeButton");
 
-menuButton.addEventListener("click", function (e) {
-  console.log(e.target);
-});
+function openSidebar() {
+  sidebar.classList.add("sidebar-active");
+  overlay.classList.remove("hidden");
+}
+
+function closeSidebar() {
+  sidebar.classList.remove("sidebar-active");
+  overlay.classList.add("hidden");
+}
+
+menuButton.addEventListener("click", openSidebar);
+closeButton.addEventListener("click", closeSidebar);
+overlay.addEventListener("click", closeSidebar);
